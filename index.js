69d93f59011dfe5548f2f1455fe4092b20419a1b
Asliddin Otabekov <126315706+asliddinotabekov@users.noreply.github.com>
2023-06-26 17:40:43 +0500

    Js entred  Variables  Data Types Document

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
-// '', "", ``
+// let data ="Lorem ipson ...... "
+// console.log(data)
 
-// let name = "Webbrain Academy";
+// let text; // o'zgaruvchi bor ammo qiymati yo'q
+// console.log(text)// undefine
+// console.log(text1)// varible yoq bolsa error beradi // "name" is not define
+// let a=b=c=3
+// console.log(a,b,c)// 3 3 3
 
-// console.log("Welcome " + name + ", IT Center");
-// console.log("Welcome \nto 'Webbrain'");
 
-// console.log(true);
-// console.log(false);
-// ==============
+let a;  // konfet yoq va qog'ozini ham tashavorgan
+console.log(a) //undefined
+console.log(typeof a) //undefined
+console.log(undefined+1) //NaN
 
-// null
-// let a;
-// let b = null;
 
-// console.log(null === undefined);
-// console.log(null == undefined);
+let b = null // konfet yoq va qog'ozi bor 🤕 
+console.log(b) // null 
+console.log(typeof b) //object 
+console.log(null+1) // 1 => yanni arifmetik amal ishlash imkoniga ega bolamiz va istalgan data typga biriktirib ketishimiz mumlin 
 
-// ==============
-// let a;
-// let b = null;
-// console.log(null + 1);
-// console.log(undefined + 1);
-// console.log(a + 1);
-// console.log(b + 1);
+console.log(a == b )//true 
+console.log(a === b )// false 
 
-// ==============
-// symbols
-// let a = Symbol("text");
-// let b = Symbol("text");
+// let a = Symbol("text")
+// let a = Symbol("text")
 
-// console.log(b === a);
-// console.log(b == a);
+// console.log(a == b )//false
+// console.log(a === b )// false 
 
-// ==============
-// Object - array, obj, function
+let arr =["Jon",12,"Doe"] // arryda malumotlar "index" bilan chaqiriladi "umumiy qismi " 
+console.log(arr)// ["Jon",12,"Doe"]
+console.log(arr[0])  //"Jon" ya'ni index orqali chaqiramiz
 
-let ar = ["Muhammad", 12, "Iqboljon"]; // index
-let obj = { name: "webbrain", status: "IT Center" }; // key
-let func = () => {};
-function test() {
-  console.log(ar[0]);
-  console.log(obj.name);
+
+let obj ={name:Jon , id: 1} // key 
+console.log(obj) //{name:Jon , id: 1}
+console.log(obj.name) // "Jon" key orqali chaqiramiz
+
+const test=()=> {
+console.log(obj)
+console.log(obj.name)
 }
+test()
 
-test();
 
-console.log(typeof func);
+// let a = 1 
+// let a = 1
+console.log(a === a ) // true "Primitive" data
+// valueni taqqoslaydi 
+console.log({b} == {a} ) // false "Non-primitive" data 
+// linkni taqqoslaydi 
+// biz Stack dagi joyni taqqoslaymiz ammo non-primitivening 
+// qiymati Heapda boladi
