1246bb9a42e5404167e945d1f7c628c0a0b993ec
Asliddin Otabekov <126315706+asliddinotabekov@users.noreply.github.com>
2023-06-26 18:27:46 +0500

    for loop anf switch case

---
+++ index.js
@@ -0,0 +1,36 @@
+// Karra jadjali 
+outer : for(i = 1; i < 10; i++ ){ 
+    console.log(`${i} Sonining kara jadvali`);
+    inter:for(j = 0; j < 10; j++ ){ 
+        console.log( `${i} x ${j} = ${i*j}`) 
+    }
+}
+
+// if -> <,>,<=,>=, ==, ===
+// switch -> ===
+// let data = "bulut";
+// switch (data) {
+//   case 1:
+//     console.log("teng");
+//     break;
+//   case "bulut":
+//     console.log("bugun bulut");
+//     break;
+//   case "qor":
+//     console.log("bugun qor");
+//     break;
+//   default:
+//     console.log("no data");
+// }
+
+
+// console.log("Hello Muhammad");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
