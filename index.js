b3a65bde7a37ba4d27d7298014b48dd91e09a6f3
Asliddin Otabekov <126315706+asliddinotabekov@users.noreply.github.com>
2023-06-26 18:26:32 +0500

    for loop anf switch case

--- index.js
+++
@@ -1,4 +0,0 @@
-
-for(i = 0; i < 10; i = i + 1 ){  // i= i+1 qachon ishlaydi ? Qachonki skop ichi birmarta yurib bo'lganda 
-	// bu nimaga qarab 10ta chiqaryapti desak ikkinchi parametr yani u true || false bolishini kutadi   
-}
