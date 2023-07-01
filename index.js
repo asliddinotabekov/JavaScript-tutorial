f7895fc7ffe72c4f60edb33a7ea95974619f0eee
Asliddin Otabekov <126315706+asliddinotabekov@users.noreply.github.com>
2023-07-01 18:08:15 +0500

    object basic Dynamic access to keys Object assign

--- index.js
+++ index.js
@@ -1,36 +1,110 @@
-// Karra jadjali 
-outer : for(i = 1; i < 10; i++ ){ 
-    console.log(`${i} Sonining kara jadvali`);
-    inter:for(j = 0; j < 10; j++ ){ 
-        console.log( `${i} x ${j} = ${i*j}`) 
-    }
-}
+    // let i = 1
+    // while(i<7){
+    //     console.log(i)
+    //     i ++ 
+    // }
 
-// if -> <,>,<=,>=, ==, ===
-// switch -> ===
-// let data = "bulut";
-// switch (data) {
-//   case 1:
-//     console.log("teng");
-//     break;
-//   case "bulut":
-//     console.log("bugun bulut");
-//     break;
-//   case "qor":
-//     console.log("bugun qor");
-//     break;
-//   default:
-//     console.log("no data");
+    // do{
+    //     console.log(i)
+    //     i--
+    // }while(i == 0)
+// let names = "Tashqi"
+//     const test =()=>{
+//          names = "Ichki"
+//         console.log(names)
 // }
+// test()
+// console.log(names)
+      
+// function func() {
+//     let a = 'Geeks';
+     
+//     if (true) {
+//          a = 'GeeksforGeeks';  // New value assigned
+//         console.log(a);
+//     }
+     
+//     console.log(a);
+// }
+// func();
+
+// var a = 'Geeks "A"';
+// let b = 'Geeks "B"';
+// function func() {
+     
+//     if (true) {
+//          a = 'Copy ""a""'; // Legal Shadowing
+//          b = 'Copy ""b""'; // Illegal Shadowing
+//         console.log(a); // It will print 'GeeksforGeeks'
+//         console.log(b); // It will print error
+//     }
+// }
+// func();
+// console.log(a)
+// console.log(b)
+
+// const telegram =(name , surname)=>{
+//     console.log(name,surname || "")
+// }
+// telegram("aslsa",)
+
+// function outer(b,c) {
+//     function inner(a=c) {
+            
+//           console.log(a+b);
+//      }
+//     return inner();
+//  }
+//  outer(11,2)
+// ================================== function with return ==============================================================//
+
+// const getGpa = (a=0,b=0,c=0,d=0)=>(a+b+c+d)/4
+
+// console.log(getGpa(12,21,21,32)) 
+// let Info ={
+//     name: "Asliddin",
+//     age : 18,
+//     founded: 2022
+//     }
+//     Info.name = "Hasan"
+//     console.log(Info.name) // Hasan
 
 
-// console.log("Hello Muhammad");
-// console.log("Hello Abdulloh");
-// console.log("Hello Abdulloh");
-// console.log("Hello Abdulloh");
-// console.log("Hello Abdulloh");
-// console.log("Hello Abdulloh");
-// console.log("Hello Abdulloh");
-// console.log("Hello Abdulloh");
-// console.log("Hello Abdulloh");
-// console.log("Hello Abdulloh");
+// let name = "Sdfafasfa"
+// let age = 12
+//     let Info ={
+//         name:name,
+//         age : age ,
+//         }
+//         console.log(Info)
+
+
+
+// const In1 ={
+//     name: "Asliddin", 
+//     age:18
+//     }
+//     const In2 ={
+//     name: "Asliddin", 
+//     age:18
+//     }
+//     const In3=In1
+
+//         console.log(In1 === In2); // false
+//         console.log(In1 === In3); // true
+    
+    const In1 ={
+        name: "Asliddin", 
+        age:18
+        }
+    
+        const In3={}
+        Object.assign(In3,In1)
+        console.log(In3)// { name: 'Asliddin', age: 18 }
+
+
+            console.log(In1 === In3); // false                              
+
+    for(let i in In1 ){
+        console.log(In1[`${i}`])
+    }
